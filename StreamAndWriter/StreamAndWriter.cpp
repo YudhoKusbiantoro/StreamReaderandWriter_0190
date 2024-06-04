0cc55a2f2f874b37b925d793350be7600d764d3e
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-06-04 10:26:57 +0700

    Menampilkan dan membaca file jika ada dan unable jika tidak ada file

--- StreamAndWriter/StreamAndWriter.cpp
+++ StreamAndWriter/StreamAndWriter.cpp
@@ -37,4 +37,21 @@ int main() {
 	ifstream infile;
 	//Menunjuk ke sebuah file
 	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan Membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//Melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//Dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//Tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//Jika tidak ditemukan file maka akan menampilkann ini
+	else cout << "Unable to open file";
+	return 0;
 }
