b4bc9d964678cd0d01d250a3f2f8cee10ccf1ea6
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-06-04 10:33:35 +0700

    Variable dengan tipe  data string

---
+++ iofileWithPath U/iofileWithPath U.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	stirng baris;
+	string namaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+}
