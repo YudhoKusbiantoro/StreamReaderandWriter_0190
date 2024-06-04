17b1d7d3ee0a9f59ff5c2e178d5e20c52cd0fe51
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-06-04 10:40:19 +0700

    Loop untuk mendapatkan, menulis, memasukan nilai dan berenti jika dimasukan karakter q

--- iofileWithPath U/iofileWithPath U.cpp
+++ iofileWithPath U/iofileWithPath U.cpp
@@ -4,16 +4,27 @@
 using namespace std;
 
 int main() {
-	stirng baris;
+	string baris;
 	string namaFile;
 
 	cout << "Masukan Nama File : ";
-	cin >> NamaFile;
+	cin >> namaFile;
 
 	//Membuka file dalam mode menulis
 	ofstream outfile;
 	//Menunjuk ke sebuah nama file
-	outfile.open(NamaFile + ".txt", ios::out);
+	outfile.open(namaFile + ".txt", ios::out);
 
 	cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "_";
+		//Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika  anda memasukan karakter q 
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
