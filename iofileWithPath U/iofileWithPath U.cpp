96da66231a9433bf419a93d3499cc9e7c7426e67
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-06-04 10:36:31 +0700

    Membuka dan menunjuk sebuah file

--- iofileWithPath U/iofileWithPath U.cpp
+++ iofileWithPath U/iofileWithPath U.cpp
@@ -9,4 +9,11 @@ int main() {
 
 	cout << "Masukan Nama File : ";
 	cin >> NamaFile;
+
+	//Membuka file dalam mode menulis
+	ofstream outfile;
+	//Menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar " << endl;
 }
